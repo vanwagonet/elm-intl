63f2d9974ca0f82d5717e8fd05d0023b56c9f240
Andy VanWagoner <andy@thetalecrafter.com>
2016-09-28 20:52:09 -0600

    Use standard js style

--- src/Native/Intl/Collator.js
+++ src/Native/Intl/Collator.js
@@ -1,132 +1,147 @@
-//import Native.List //
-//import Intl.Locale //
+// import Native.List //
+// import Intl.Locale //
 
+/* global
+  F3
+  _elm_lang$core$Native_List
+  _thetalecrafter$elm_intl$Intl_Locale$Locale
+  _thetalecrafter$elm_intl$Intl_Collator$Search
+  _thetalecrafter$elm_intl$Intl_Collator$Sort
+  _thetalecrafter$elm_intl$Intl_Collator$Base
+  _thetalecrafter$elm_intl$Intl_Collator$Accent
+  _thetalecrafter$elm_intl$Intl_Collator$Case
+  _thetalecrafter$elm_intl$Intl_Collator$Variant
+  _thetalecrafter$elm_intl$Intl_Collator$Upper
+  _thetalecrafter$elm_intl$Intl_Collator$Lower
+  _thetalecrafter$elm_intl$Intl_Collator$Default
+*/
+/* eslint-disable camelcase */
+
+// eslint-disable-next-line
 var _thetalecrafter$elm_intl$Native_Intl_Collator = function () {
-  
-// this will create an early error if Intl is not supported
-var Collator = Intl.Collator
+  // this will create an early error if Intl is not supported
+  var Collator = Intl.Collator
 
-function usageToUnion (value) {
-  switch (value) {
-    case 'search':
-      return _thetalecrafter$elm_intl$Intl_Collator$Search
-    case 'sort':
-    default:
-      return _thetalecrafter$elm_intl$Intl_Collator$Sort
+  function usageToUnion (value) {
+    switch (value) {
+      case 'search':
+        return _thetalecrafter$elm_intl$Intl_Collator$Search
+      case 'sort':
+      default:
+        return _thetalecrafter$elm_intl$Intl_Collator$Sort
+    }
   }
-}
 
-function usageFromUnion (value) {
-  switch (value) {
-    case _thetalecrafter$elm_intl$Intl_Collator$Search:
-      return 'search'
-    case _thetalecrafter$elm_intl$Intl_Collator$Sort:
-    default:
-      return 'sort'
+  function usageFromUnion (value) {
+    switch (value) {
+      case _thetalecrafter$elm_intl$Intl_Collator$Search:
+        return 'search'
+      case _thetalecrafter$elm_intl$Intl_Collator$Sort:
+      default:
+        return 'sort'
+    }
   }
-}
 
-function sensitivityToUnion (value) {
-  switch (value) {
-    case 'base':
-      return _thetalecrafter$elm_intl$Intl_Collator$Base
-    case 'accent':
-      return _thetalecrafter$elm_intl$Intl_Collator$Accent
-    case 'case':
-      return _thetalecrafter$elm_intl$Intl_Collator$Case
-    case 'variant':
-    default:
-      return _thetalecrafter$elm_intl$Intl_Collator$Variant
+  function sensitivityToUnion (value) {
+    switch (value) {
+      case 'base':
+        return _thetalecrafter$elm_intl$Intl_Collator$Base
+      case 'accent':
+        return _thetalecrafter$elm_intl$Intl_Collator$Accent
+      case 'case':
+        return _thetalecrafter$elm_intl$Intl_Collator$Case
+      case 'variant':
+      default:
+        return _thetalecrafter$elm_intl$Intl_Collator$Variant
+    }
   }
-}
 
-function sensitivityFromUnion (value) {
-  switch (value) {
-    case _thetalecrafter$elm_intl$Intl_Collator$Base:
-      return 'base'
-    case _thetalecrafter$elm_intl$Intl_Collator$Accent:
-      return 'accent'
-    case _thetalecrafter$elm_intl$Intl_Collator$Case:
-      return 'case'
-    case _thetalecrafter$elm_intl$Intl_Collator$Variant:
-    default:
-      return 'variant'
+  function sensitivityFromUnion (value) {
+    switch (value) {
+      case _thetalecrafter$elm_intl$Intl_Collator$Base:
+        return 'base'
+      case _thetalecrafter$elm_intl$Intl_Collator$Accent:
+        return 'accent'
+      case _thetalecrafter$elm_intl$Intl_Collator$Case:
+        return 'case'
+      case _thetalecrafter$elm_intl$Intl_Collator$Variant:
+      default:
+        return 'variant'
+    }
   }
-}
 
-function caseFirstToUnion (value) {
-  switch (value) {
-    case 'upper':
-      return _thetalecrafter$elm_intl$Intl_Collator$Upper
-    case 'lower':
-      return _thetalecrafter$elm_intl$Intl_Collator$Lower
-    case 'false':
-    default:
-      return _thetalecrafter$elm_intl$Intl_Collator$Default
+  function caseFirstToUnion (value) {
+    switch (value) {
+      case 'upper':
+        return _thetalecrafter$elm_intl$Intl_Collator$Upper
+      case 'lower':
+        return _thetalecrafter$elm_intl$Intl_Collator$Lower
+      case 'false':
+      default:
+        return _thetalecrafter$elm_intl$Intl_Collator$Default
+    }
   }
-}
 
-function caseFirstFromUnion (value) {
-  switch (value) {
-    case _thetalecrafter$elm_intl$Intl_Collator$Upper:
-      return 'upper'
-    case _thetalecrafter$elm_intl$Intl_Collator$Lower:
-      return 'lower'
-    case _thetalecrafter$elm_intl$Intl_Collator$Default:
-    default:
-      return 'false'
+  function caseFirstFromUnion (value) {
+    switch (value) {
+      case _thetalecrafter$elm_intl$Intl_Collator$Upper:
+        return 'upper'
+      case _thetalecrafter$elm_intl$Intl_Collator$Lower:
+        return 'lower'
+      case _thetalecrafter$elm_intl$Intl_Collator$Default:
+      default:
+        return 'false'
+    }
   }
-}
 
-function fromLocale (locale) {
-  return new Collator(locale._0)
-}
-
-function fromOptions (record) {
-  var locale = record.locale._0
-  var options = {
-    usage: usageFromUnion(record.usage),
-    sensitivity: sensitivityFromUnion(record.sensitivity),
-    ignorePunctuation: record.ignorePunctuation,
-    numeric: record.numeric,
-    caseFirst: caseFirstFromUnion(record.caseFirst)
+  function fromLocale (locale) {
+    return new Collator(locale._0)
   }
-  return new Collator(locale, options)
-}
 
-function compare (collator, string1, string2) {
-  var value = collator.compare(string1, string2)
-  return { ctor: value === 0 ? 'EQ' : value < 0 ? 'LT' : 'GT' }
-}
-
-function resolvedOptions (collator) {
-  var options = collator.resolvedOptions()
-  return {
-    locale: _thetalecrafter$elm_intl$Intl_Locale$Locale(options.locale),
-    usage: usageToUnion(options.usage),
-    sensitivity: sensitivityToUnion(options.sensitivity),
-    ignorePunctuation: options.ignorePunctuation,
-    numeric: options.numeric,
-    caseFirst: caseFirstToUnion(options.caseFirst)
+  function fromOptions (record) {
+    var locale = record.locale._0
+    var options = {
+      usage: usageFromUnion(record.usage),
+      sensitivity: sensitivityFromUnion(record.sensitivity),
+      ignorePunctuation: record.ignorePunctuation,
+      numeric: record.numeric,
+      caseFirst: caseFirstFromUnion(record.caseFirst)
+    }
+    return new Collator(locale, options)
   }
-}
 
-function supportedLocalesOf (list) {
-  var array = _elm_lang$core$Native_List.toArray(list).map(function (locale) {
-    return locale._0
-  })
-  var supported = Collator.supportedLocalesOf(array).map(
+  function compare (collator, string1, string2) {
+    var value = collator.compare(string1, string2)
+    return { ctor: value === 0 ? 'EQ' : value < 0 ? 'LT' : 'GT' }
+  }
+
+  function resolvedOptions (collator) {
+    var options = collator.resolvedOptions()
+    return {
+      locale: _thetalecrafter$elm_intl$Intl_Locale$Locale(options.locale),
+      usage: usageToUnion(options.usage),
+      sensitivity: sensitivityToUnion(options.sensitivity),
+      ignorePunctuation: options.ignorePunctuation,
+      numeric: options.numeric,
+      caseFirst: caseFirstToUnion(options.caseFirst)
+    }
+  }
+
+  function supportedLocalesOf (list) {
+    var array = _elm_lang$core$Native_List.toArray(list).map(function (locale) {
+      return locale._0
+    })
+    var supported = Collator.supportedLocalesOf(array).map(
     _thetalecrafter$elm_intl$Intl_Locale$Locale
   )
-  return _elm_lang$core$Native_List.fromArray(supported)
-}
+    return _elm_lang$core$Native_List.fromArray(supported)
+  }
 
-return {
-  fromLocale: fromLocale,
-  fromOptions: fromOptions,
-  compare: F3(compare),
-  resolvedOptions: resolvedOptions,
-  supportedLocalesOf: supportedLocalesOf
-}
-
-}();
+  return {
+    fromLocale: fromLocale,
+    fromOptions: fromOptions,
+    compare: F3(compare),
+    resolvedOptions: resolvedOptions,
+    supportedLocalesOf: supportedLocalesOf
+  }
+}()
--- src/Native/Intl/Locale.js
+++ src/Native/Intl/Locale.js
@@ -1,43 +1,49 @@
-//import Maybe //
+// import Maybe //
 
+/* global
+  _elm_lang$core$Maybe$Just
+  _thetalecrafter$elm_intl$Intl_Locale$Locale
+  _elm_lang$core$Maybe$Nothing
+*/
+/* eslint-disable camelcase */
+
+// eslint-disable-next-line
 var _thetalecrafter$elm_intl$Native_Intl_Locale = function () {
+  // this will create an early error if Intl is not supported
+  var checker = Intl.NumberFormat
 
-// this will create an early error if Intl is not supported
-var checker = Intl.NumberFormat
-
-function normalize (tag) {
-  var parts = tag.toLowerCase().split('-')
-  var index = 0
-  var canHaveExtLang = index < parts.length && parts[index].length <= 3
-  if (canHaveExtLang) {
-    while (++index < parts.length && parts[index].length === 3);
+  function normalize (tag) {
+    var parts = tag.toLowerCase().split('-')
+    var index = 0
+    var canHaveExtLang = index < parts.length && parts[index].length <= 3
+    if (canHaveExtLang) {
+      while (++index < parts.length && parts[index].length === 3);
+    }
+    var isScript = index < parts.length && parts[index].length === 4
+    if (isScript) {
+      parts[index] = parts[index][0].toUpperCase() + parts[index].slice(1)
+      ++index
+    }
+    var isRegion = index < parts.length && parts[index].length === 2
+    if (isRegion) {
+      parts[index] = parts[index].toUpperCase()
+    }
+    return parts.join('-')
   }
-  var isScript = index < parts.length && parts[index].length === 4
-  if (isScript) {
-    parts[index] = parts[index][0].toUpperCase() + parts[index].slice(1)
-    ++index
-  }
-  var isRegion = index < parts.length && parts[index].length === 2
-  if (isRegion) {
-    parts[index] = parts[index].toUpperCase()
-  }
-  return parts.join('-')
-}
 
-function fromString (tag) {
-  try {
-    var supported = checker.supportedLocalesOf([ tag ]) // throws if the tag is invalid
-    // use the normalized tag, (doesn't do canonicalization like ji -> yi)
-    return _elm_lang$core$Maybe$Just(
-      _thetalecrafter$elm_intl$Intl_Locale$Locale(normalize(tag))
-    )
-  } catch (e) {
-    return _elm_lang$core$Maybe$Nothing
+  function fromString (tag) {
+    try {
+      checker.supportedLocalesOf([ tag ]) // throws if the tag is invalid
+      // use the normalized tag, (doesn't do canonicalization like ji -> yi)
+      return _elm_lang$core$Maybe$Just(
+        _thetalecrafter$elm_intl$Intl_Locale$Locale(normalize(tag))
+      )
+    } catch (e) {
+      return _elm_lang$core$Maybe$Nothing
+    }
   }
-}
 
-return {
-  fromString: fromString
-}
-
-}();
+  return {
+    fromString: fromString
+  }
+}()
